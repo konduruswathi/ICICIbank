ae6f24ef5358f1810f3e1348fff4c8c78d634f67
konduruswathi <42470121+konduruswathi@users.noreply.github.com>
2018-10-03 06:05:53 +0000

    Delete CustomerNotFoundException.java

--- src/main/java/com/capgemini/icicibank/exception/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.icicibank.exception;
-
-public class CustomerNotFoundException extends RuntimeException{
-public CustomerNotFoundException(String message) {
-	super(message);
-}
-}
